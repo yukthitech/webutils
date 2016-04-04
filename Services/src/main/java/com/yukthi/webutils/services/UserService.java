7f4ad0b4de2804a02423746802bfabc383617a71
Kranthi <akranthi@yukthi-tech.co.in>
2016-04-04 09:40:57 +0530

    Reverting user service changes

--- Services/src/main/java/com/yukthi/webutils/services/UserService.java
+++ Services/src/main/java/com/yukthi/webutils/services/UserService.java
@@ -44,48 +44,48 @@ public class UserService extends BaseCrudService<UserEntity, IUserRepository>
 		return super.repository.fetchPassword(userName, ownerType, ownerId);
 	}
 	
-	/* *
+	/**
 	 * Fetches user with specified details.
 	 * @param userName User name
 	 * @param ownerType Owner type under which user is registered
 	 * @param ownerId Owner id under which user is registered
 	 * @return Matching user details
-	 * /
+	 */
 	public UserEntity getUser(String userName, String ownerType, long ownerId)
 	{
 		return super.repository.fetchUser(userName, ownerType, ownerId);
 	}
 	
-	/* *
+	/**
 	 * Fetches user based on base entity details.
 	 * @param baseEntityType Base entity type for which this user is created
 	 * @param baseEntityId Base entity id for which this user is created
 	 * @return Matching user entity
-	 * /
+	 */
 	public UserEntity fetchUserByBaseEntity(String baseEntityType, long baseEntityId)
 	{
 		return super.repository.fetchUserByBaseEntity(baseEntityType, baseEntityId);
 	}
 
-	/* *
+	/**
 	 * Marks user as deleted based on specified base entity type and id.
 	 * @param baseEntityType Base entity type for which this user is created
 	 * @param baseEntityId Base entity id for which this user is created
 	 * @return true if deletion was successful
-	 * /
+	 */
 	public boolean deleteByBaseEntity(String baseEntityType, long baseEntityId)
 	{
 		return super.repository.markDeletedByBaseEntity(baseEntityType, baseEntityId, true);
 	}
 	
-	/* *
+	/**
 	 * Updates password of user under specified ownership and with specified user name.
 	 * @param ownerType
 	 * @param ownerId
 	 * @param userName
 	 * @param password
 	 * @return
-	 * /
+	 */
 	public boolean updatePassword(String ownerType, long ownerId, String userName, String password)
 	{
 		return super.repository.updatePassword(ownerType, ownerId, userName, password);
